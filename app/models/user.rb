92f91e4712d969a378baa9502f5eb324c8ae5e26
mrevilpig <wadeliao1986@172-26-207-49.dynapool.nyu.edu>
2014-05-29 16:06:12 -0400

    Add projects/intro view and github/logdown links in navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ActiveRecord::Base
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
     user.username = auth.info.name   # assuming the user model has a name
+    user.address = auth.info.location
     #user.image = auth.info.image # assuming the user model has an image
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Devise::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root 'users#index'
+  root 'welcome#index'
 
+  get '/projects', to: 'projects#index'
+  get '/intro', to: 'users#index'
 end
